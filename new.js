e91f72766bfb30d63b32b106327a746b88c847da
quantum-bonney <sbonney@quantumgroupgh.com>
2025-05-20 11:45:54 +0000

    get started with javascript for node

---
+++ new.js
@@ -0,0 +1,20 @@
+console.log("Hell0 World!!");
+let interest=0.25;
+let principal=1000;
+
+let account={
+    name : "Samuel Bonney",
+    balance: 28901,
+    is_married: false
+
+}
+
+account.name="Salma Paralluelo";
+account.gender="Female";
+
+
+let colors=['blue','red','green','grey'];
+
+colors[7]='gold';
+console.log(colors.length)
+
